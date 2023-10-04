c5fe51ccaaae018055a2947fa97296bb38d750f8
Gus Reid <gu5to@proton.me>
2023-10-04 08:32:02 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layouts/*.html",
+    "./layouts/index.html",
+    "./layouts/default/*.html",
+    "./layouts/partials/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
+
